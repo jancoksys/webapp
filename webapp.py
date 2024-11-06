90c7942a9189cc204538b3ac0da92af41f86dd92
Yuri Sysenko <Yura.Sysenko@yandex.ru>
2024-11-06 22:40:09 +1000

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,10 +8,7 @@ API_URL = '/static/openapi.json'
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Test application"
-    }
+    API_URL
 )
 
 app.register_blueprint(swaggerui_blueprint)
